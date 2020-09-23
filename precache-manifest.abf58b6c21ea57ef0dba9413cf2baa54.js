b2ebc12bd70d555d6fb9f09af8dbc122163fa0e0
siu-sing <lai.siusing@gmail.com>
2020-09-23 16:23:07 +0800

    Updates

---
+++ precache-manifest.abf58b6c21ea57ef0dba9413cf2baa54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba7ddaa0921d300607fd72316086c90",
+    "url": "/qsa/index.html"
+  },
+  {
+    "revision": "bfe57ceab0f27dbbe1d0",
+    "url": "/qsa/static/css/main.489b4ff1.chunk.css"
+  },
+  {
+    "revision": "ad167841522e0fa2907b",
+    "url": "/qsa/static/js/2.3150db49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qsa/static/js/2.3150db49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe57ceab0f27dbbe1d0",
+    "url": "/qsa/static/js/main.cc554963.chunk.js"
+  },
+  {
+    "revision": "6b70b7258106d87a70f4",
+    "url": "/qsa/static/js/runtime-main.3e99aaee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qsa/static/media/logo.5d5d9eef.svg"
+  }
+]);
